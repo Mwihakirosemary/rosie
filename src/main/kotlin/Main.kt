6aa91c162d45896a2dd3053f229d5c94c53999a3
Mundurujoana <mundujoana@gmail.com>
2022-02-14 22:55:23 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var name:String = "Rosemary Mwihaki"
+    println(name)
+    var age:Int = 23
+    println(age)
+    var myNumber:Long = 254799864112L
+    println(myNumber)
+    var weight:Float = 54.2F
+    println(weight)
+    val kenyan:Boolean = true
+    println(kenyan)
+}
